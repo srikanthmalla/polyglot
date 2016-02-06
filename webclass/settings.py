a97f0bc8becb4050c21dc374262b82db198bf9b2
srikanthmalla <malla.srikanth2012@vit.ac.in>
2016-02-05 20:20:09 -0500

    static serving

--- webclass/settings.py
+++ webclass/settings.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join('app/simpleapp1/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
