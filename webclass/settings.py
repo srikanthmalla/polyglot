5a688f115819737776bce34af09d5f6a9d72d787
srikanthmalla <malla.srikanth2012@vit.ac.in>
2016-02-05 20:01:28 -0500

    static dirs

--- webclass/settings.py
+++ webclass/settings.py
@@ -111,7 +111,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/app/simpleapp1/static'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join('/app/simpleapp1/static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join('/app/simpleapp1/static'),
+# )
 
